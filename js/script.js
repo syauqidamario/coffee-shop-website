68c7efdee672ddfd2f0e1cc6ec3365b742b2ea96
syauqidamario <syauqi.djohan@student.umn.ac.id>
2023-10-23 18:11:38 +0700

    Started making the web more responsive and added search function and product page.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
 // Toggle class active
 const navbarNav = document.querySelector('.navbar-nav');
 
-// ketika hamburger menu di klik
+// When the hamburger menu is clicked
 document.querySelector('#hamburger-menu').onclick = () => {
       navbarNav.classList.toggle("active");
 };
 
-// Klik di luar sidebar untuk menghilangkan nav 
-const hamburger = document.querySelector('#hamburger-menu');
+// Toggle class active for the search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = () => 
+{
+      searchForm.classList.toggle('active');
+      searchBox.focus();
+      e.preventDefault();
+}
+
+
+// Click outside of the element
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
 
 document.addEventListener('click', function(e) {
-      if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+      if(!hm.contains(e.target) && !navbarNav.contains(e.target)){
+            navbarNav.classList.remove('active');
+      }
+      if(!sb.contains(e.target) && !searchForm.contains(e.target)){
             navbarNav.classList.remove('active');
       }
 });
