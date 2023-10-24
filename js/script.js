6d84a3c453474e349c134ea7f229ca0ba9e3e092
syauqidamario <syauqi.djohan@student.umn.ac.id>
2023-10-24 17:34:45 +0700

    Almost done. Will be refined soon.

--- js/script.js
+++ js/script.js
@@ -17,10 +17,18 @@ document.querySelector('#search-button').onclick = () =>
       e.preventDefault();
 };
 
+// Toggle class active for shopping cart
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+      shoppingCart.classList.toggle('active');
+      e.preventDefault();
+}
+
 
 // Click outside of the element
 const hm = document.querySelector('#hamburger-menu');
 const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shopping-cart-button');
 
 document.addEventListener('click', function(e) {
       if(!hm.contains(e.target) && !navbarNav.contains(e.target)){
@@ -29,4 +37,41 @@ document.addEventListener('click', function(e) {
       if(!sb.contains(e.target) && !searchForm.contains(e.target)){
             navbarNav.classList.remove('active');
       }
+       if(!sc.contains(e.target) && !navbarNav.contains(e.target)){
+            navbarNav.classList.remove('active');
+      }
 });
+
+// Box modal
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelector('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+      btn.onclick = (e) => 
+{
+      itemDetailModal.style.display = 'flex';
+      e.preventDefault();
+};
+});
+
+itemDetailButtons.onclick = (e) => {
+      itemDetailModal.computedStyleMap.display = 'flex';
+      e.preventDefault();
+};
+
+// Click close modal button
+document.querySelector('.modal .close-icon').onclick = (e) => 
+{
+      itemDetailModal.style.display = 'none';
+      e.preventDefault();
+};
+
+// Click outside of modal
+const modal = document.querySelector('#item-detail-modal');
+window.onclick = (e) =>
+{
+      if(e.target === modal)
+      {
+            modal.style.display = 'none';
+      }
+};
